c5d44644f5600c5548c45de3fb4eb1db82cfc443
YacineMK <merroukiyacine06@gmail.com>
2024-06-13 23:58:30 +0100

    add css vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': 'var(--bg)',
+        'textColor': 'var(--color)',
+        'customBlue': 'var(--blue)',
+      },
+    },
   },
   plugins: [],
 }
