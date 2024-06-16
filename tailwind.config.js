65e561967fa2045c24e171371d8d0b3f3b9b73e8
Younesfdj <ferradjiyounes69@gmail.com>
2024-06-16 17:41:03 +0100

    fix: fixed tailwind config error

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
       },
       fontFamily: {
         CinzelDecorative: ["Cinzel Decorative", "serif"],
+        font: ["Cinzel"],
       },
       backgroundImage: {
         registerBackground:
@@ -22,9 +23,6 @@ export default {
       boxShadow: {
         custom:
           "0px 0px 6.5px 0px var(--Color, #4D7292), 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-      },
-      fontFamily: {
-        font: ["Cinzel"],
       },
       dropShadow: {
         custom1: "0 -1mm 4mm #5fd8ff",
