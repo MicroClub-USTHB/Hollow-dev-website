86db8cdb02ee3a0a4b3716195ba8c42cc6a48dec
Naaylla <lulita181910@hotmail.fr>
2024-06-16 22:35:38 +0100

    adding card decoration and content

--- src/components/FAQ/FaqItem.tsx
+++ src/components/FAQ/FaqItem.tsx
@@ -1,24 +1,33 @@
-import ItLogo from '../../assets/itlogo.svg'
-import Logo from '../../assets/Logo.png'
+import ItLogo from '../../assets/itlogo.svg';
+import Borders from '../../assets/borders.png';
+
 
 function FaqItem() {
     return (
-            <div className='w-60 h-80 cursor-pointer group perspective'>
-                <div className='relative bg-[#07080D] preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000 rounded-lg'>
-                    <div className='absolute backface-hidden w-full h-full'>
-                        <img src={ItLogo} className='absolute inset-0 m-auto items-center'/>
-                    </div>
-                    <div className='absolute my-rotate-y-180 backface-hidden w-full h-full bg-gray-100 rounded-lg'>
-                        <div className="text-center flex flex-col items-center justify-center h-full text-gray-800 px-2 pb-24">
-                            <button
-                                className="bg-teal-500 px-6 py-2 font-semibold text-white rounded-full absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-20 scale-0 group-hover:scale-125">
-                                Read more
-                            </button>
-                        </div>  
+        <div className='w-60 h-80 cursor-pointer group perspective'>
+         
+            <div className='relative bg-[#07080D] preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000 rounded-lg'>
+                <div className='absolute inset-0 flex items-center justify-center'>
+                    <img src={Borders} className='w-full h-full object-cover m-2' alt='Borders' />
+                </div>
+                <div className='absolute flex flex-col items-center backface-hidden w-full h-full space-y-9'>
+                    <img src={ItLogo} className='w-12 h-12 mt-5'/>
+                
+                    <div className='border border-white rounded-lg p-8 m-8 bg-[#0D0E13]'>
+                        <p className='text-xs text-white text-center'>Do I have to be a top talent to participate?</p>
                     </div>
                 </div>
+                <div className='absolute my-rotate-y-180 backface-hidden w-full h-full bg-[#07080D] rounded-lg'>
+                    <div className='absolute inset-0 flex items-center justify-center'>
+                        <img src={Borders} className='w-full h-full object-cover m-2' alt='Borders' />
+                    </div>
+                    <div className="m-8 flex items-center justify-center h-full text-gray-800 px-2 pb-24">
+                        <p className='text-xs text-white text-center'>Do I have to be a top talent to participate?</p>
+                    </div>  
+                </div>
             </div>
-    )
+        </div>
+    );
 }
 
 export default FaqItem;
