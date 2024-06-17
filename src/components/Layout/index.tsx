a82fac99e6040669e9c3e495ea4a0515c1f52e6a
SamiSelx <samifcb14@gmail.com>
2024-06-17 14:53:30 +0200

    add loading to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import { ReactNode,useState } from "react";
 import Navbar from "./navbar";
 import { Toaster } from "react-hot-toast";
 import useNotAuthorized from "../../hooks/useNotAuthorized";
 import useGetUser from "../../hooks/useGetUser";
+import Loading from "../UI/Loading";
 
 type Props = {
   children: ReactNode;
@@ -13,17 +14,24 @@ export default function Layout({ children }: Props) {
   console.log(error);
   const [user,waiting] = useGetUser();
 
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
+
 
   // Waiting data to render the whole page
  if(!waiting){
   return (
-    <>
+    loading ? <Loading/> : (
+      <>
       <Toaster />
       <div>
         <Navbar user={user}/>
         {children}
       </div>
     </>
+    )
   );
  }
 }
