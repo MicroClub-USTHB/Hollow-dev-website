5be25f9ad7063f44e9b596bfce0dbfb928d68a58
ramzy1453 <kemmounramzy93@gmail.com>
2024-06-17 02:07:24 +0100

    merged with faq-nayla

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import About from "../../components/About";
 import Loading from "../../components/UI/Loading";
 import Layout from "../../components/Layout";
+import Faq from "../../components/FAQ";
 export default function Home() {
   const [loading, setLoading] = useState(true);
   setTimeout(() => {
@@ -14,6 +15,7 @@ export default function Home() {
       ) : (
         <Layout>
           <About />
+          <Faq />
         </Layout>
       )}
     </div>
