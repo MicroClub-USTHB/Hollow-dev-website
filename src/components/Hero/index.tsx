45bce029045724f9a95fa9c937d9d74c24342e4e
ramzy1453 <kemmounramzy93@gmail.com>
2024-06-18 22:33:30 +0100

    added trailer in hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
+import HollowButton from "../UI/HollowButton";
+import "../../assets/styles/trailer.css";
 const Hero: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center text-white">
-      <div className="text-center mb-10">
+      <div className="text-center mb-16">
         <h1 className="text-5xl font-bold mt-4 font-sedan">HOLLOW DEV</h1>
         <p className="text-lg mt-2 font-sedan">
           Let’s conquer the web kingdom!
@@ -13,24 +13,19 @@ const Hero: React.FC = () => {
           alt="hero-logo"
           className="w-32 mt-1 mx-auto absolute left-80 transform -translate-x-1/3"
         />
-
-        <button className="group relative overflow-hidden rounded bg-[#0E1324] px-8 py-2 text-white transition-all duration-300 ease-out hover:bg-gradient-to-r mt-6 hover:from-[#0E1324] hover:to-[#1F2937] ring-1 hover:ring-[#0E1324] ring-offset-1">
-          Register Now
-        </button>
+        <div className="bg-transparent text-white flex items-center justify-center text-xl mt-4">
+          <HollowButton title="Register Now" />
+        </div>
       </div>
 
-      <div className="relative w-100 " style={{ height: "700px" }}>
-        <img
-          src="/src/assets/hero-browser.png"
-          alt="hero-browser"
-          className="w-full h-full rounded-lg object-cover"
-        />
-        <img
-          src="/src/assets/hero-code.png"
-          alt="hero-code"
-          className="absolute bottom-1 left-1/2 transform -translate-x-1/2  "
-          style={{ width: "800px", height: "260px", borderRadius: "20px" }}
-        />
+      <div className="flex justify-center items-center">
+        <iframe
+          src="https://drive.google.com/file/d/17QOtAYThN9_6_OR4PLUVksu4cBJb7fLw/preview"
+          width="800"
+          height="480"
+          className="border border-[#3D3D3D] rounded-xl trailer-iframe"
+          allow="autoplay"
+        ></iframe>
       </div>
     </div>
   );
