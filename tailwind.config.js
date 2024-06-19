fd6a4766de51cb04443efeaa654e0dc17e7dd65e
SamiSelx <samifcb14@gmail.com>
2024-06-19 19:01:40 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
       },
       backgroundImage: {
         registerBackground:
-          "url('/src/assets/registration/Registrationbg.svg')",
+          "url('/src/assets/registration/Registrationbg.png')",
       },
       colors: {
         dark: "var(--bg)",
@@ -28,6 +28,7 @@ export default {
         custom:
           "0px 0px 6.5px 0px var(--Color, #4D7292), 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       },
+
       dropShadow: {
         custom1: "0 -1mm 4mm #5fd8ff",
         custom2: "0 -1mm 4mm #4d7292",
