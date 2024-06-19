1a5965c88fad3770529e6b77b881d9329655234b
SamiSelx <samifcb14@gmail.com>
2024-06-19 19:01:16 +0200

    add error toast

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { UserI } from "../types/types";
 import clearSession from "../utils/clearSession.ts";
 import { useUser } from "./useUser.ts";
-
+import toast from "react-hot-toast";
 type UseGetUserResult = [UserI | null, boolean];
 
 export default function useGetUser(): UseGetUserResult {
@@ -21,9 +21,24 @@ export default function useGetUser(): UseGetUserResult {
         if (data.data) {
           // If user not On MC server then clear Cookies and Session From DB
           if (data.data.status == "failed") {
+            
+            setTimeout(() => {
+              toast("You must be a member of the Micro Club to register.", {
+                position: "top-center",
+                className: "mt-12",
+                style: {
+                  textAlign: "center",
+                  color: "white",
+                  backgroundColor: "#00020e",
+                  border: "1px solid white",
+                  fontSize: "1.25rem",
+                  lineHeight: "1.5",
+                },
+              });
+            }, 3300);
             await clearSession(data.data);
             setUser(null);
-          } else setUser(data.data);
+          } else setUser({...data.data,successRegistre:false});
         }
       } catch (error) {
         console.log(error);
