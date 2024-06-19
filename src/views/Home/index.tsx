fc091b54f2908466acb4c2f3836917b1632caf01
SamiSelx <samifcb14@gmail.com>
2024-06-19 18:59:29 +0200

    add success Toast

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,26 @@
-
+import { useEffect } from "react";
 import About from "../../components/About";
+import { useUser } from "../../hooks/useUser";
+import toast from "react-hot-toast";
 export default function Home() {
+  const {user,setUser} = useUser()
+  
+  useEffect(()=>{
+    if(user?.successRegistre){
+      toast('Your Registre Has Been Successfully Recieved',{
+        className:"mt-12",
+        style: {
+          textAlign: "center",
+          color: "white",
+          backgroundColor: "#00020e",
+          border: "1px solid white",
+          fontSize: "1.25rem",
+          lineHeight:'1.5',
+        },
+      })
+      setUser({...user,successRegistre:!user.successRegistre})
+    }
+  },[]) 
   return (
     <div className="pt-20 bg-dark">
       <About/>
