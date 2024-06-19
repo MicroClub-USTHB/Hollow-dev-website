e4ae62be4e8c96354d0f36c1b201655c1c6c4aa0
ramzy1453 <kemmounramzy93@gmail.com>
2024-06-19 17:31:25 +0100

    fixed navbar responsive

--- src/components/UI/HollowButton.tsx
+++ src/components/UI/HollowButton.tsx
@@ -4,7 +4,7 @@ type Props = {
   title: string;
   onClick?: () => void;
   href?: string;
-  icon?: string; 
+  icon?: string;
 };
 
 export default function HollowButton(props: Props) {
@@ -14,14 +14,14 @@ export default function HollowButton(props: Props) {
         <Link
           to={props.href || "/"}
           onClick={props.onClick}
-          className="sm:hidden block absolute overflow-hidden w-12 h-12 absolute right-0 -mt-3 mr-6 hover:opacity-80"
+          className="md:hidden block overflow-hidden w-12 h-12 absolute right-0 -mt-3 mr-6 hover:opacity-80"
           style={{ backgroundImage: `url(${props.icon})` }}
         />
       ) : (
         <Link
           to={props.href || "/"}
           onClick={props.onClick}
-          className="hidden sm:block group relative overflow-hidden rounded bg-[#0E1324] px-8 py-2 text-white transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-[#0E1324] hover:to-[#1F2937] ring-1 hover:ring-[#0E1324] ring-offset-1"
+          className="hidden md:block group relative overflow-hidden rounded bg-[#0E1324] px-8 py-2 text-white transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-[#0E1324] hover:to-[#1F2937] ring-1 hover:ring-[#0E1324] ring-offset-1"
         >
           <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-700 group-hover:-translate-x-40"></span>
           <span className="relative">{props.title}</span>
