0ab3b29199688ebbcbbc6dd9954998a0cb910d29
SamiSelx <samifcb14@gmail.com>
2024-06-20 02:08:22 +0200

    update header and footer registration page

--- src/components/Registration/Footer.tsx
+++ src/components/Registration/Footer.tsx
@@ -1,8 +1,15 @@
+import itLogo from "../../assets/itlogo.svg";
+import donw from "../../assets/registration/down.svg";
 
 const Footer = () => {
   return (
-    <div className="text-white">Footer</div>
-  )
-}
+    <>
+      <div className="mt-4 md:mt-0">
+        <img src={donw} alt=" overtitle" className=" w-full" />
+      </div>
+      <img src={itLogo} width={72} alt="it section logo" className=" " />
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
