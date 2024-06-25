50d4e46539b67cff3996323d57bb091fceb47e97
saiber-elite <jalilotao@gmail.com>
2024-06-26 00:06:04 +0100

    fixed cards and added back button

--- src/views/Challenges/card.jsx
+++ src/views/Challenges/card.jsx
@@ -1,4 +1,3 @@
-
 import Down from '../../assets/about-down.png'
 export default function Card(props) {
   return(
@@ -7,7 +6,7 @@ export default function Card(props) {
       <div className=' absolute top-2 left-3 text-2xl font-sedan'>
         {props.id}
       </div>
-      <div className={`flex flex-col justify-center items-center absolute ${props.color} w-40 rounded-md bg-opacity-55 bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-2 `}>
+      <div className={`flex flex-col justify-center items-center absolute ${props.color} w-40 rounded-md bg-opacity-45 bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-2 `}>
         <h1 className=' font-font text-center text-sm'>{props.name}</h1>
         <img src={Down}/>
       </div>
--- src/views/Challenges/index.tsx
+++ src/views/Challenges/index.tsx
@@ -1,77 +1,101 @@
-// import Hollow from '../../assets/challenges/hollow.svg'
+import Hollow from '../../assets/challenges/hollow.svg'
 // import Ghost from '../../assets/challenges/ghost.svg'
+import MC from '../../assets/mcLogo.svg'
+import IT from '../../assets/itlogo.svg'
+import Arrow from '../../assets/challenges/Vector.svg'
 import Down from '../../assets/about-down.png'
 import Card from '../Challenges/card.jsx'
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 export default function Challenges() {
   
-  const [cards , setCards] = useState([
+  const cards = [
     {
       id : "I",
       name : "False knight",
-      url: "./img/1.jpg",
-      color: "bg-red-300"
+      url: "./challenge images/1.jpg",
+      color: "bg-black"
     },
     {
-      "id" : "II",
-      "name" : "Hornet",
-      url: "./2.jpg"
+      id : "II",
+      name : "Hornet",
+      url: "./challenge images/2.jpg",
+      color: "bg-stone-950"
     },
     {
-      "id" : "III",
-      "name" : "Mantis Lords",
-      url: "./3.jpg"
+      id : "III",
+      name : "Mantis Lords",
+      url: "./challenge images/3.jpg",
+      color: "bg-green-950"
     },
     {
-      "id" : "IV",
-      "name" : "Watcher knight",
-      url: "./4.jpg"
+      id : "IV",
+      name : "Watcher knight",
+      url: "./challenge images/4.jpg",
+      color: "bg-dark"
     },
     {
-      "id" : "IV",
-      "name" : "Broken Vessel",
-      url: "./5.jpg"
+      id : "IV",
+      name : "Broken Vessel",
+      url: "./challenge images/5.jpg",
+      color: "bg-orange-950"
     },
     {
-      "id" : "IV",
-      "name" : "Traitor Lord",
-      url: "./6.jpg"
+      id : "IV",
+      name : "Traitor Lord",
+      url: "./challenge images/6.jpg",
+      color: "bg-blue-950"
     },
     {
-      "id" : "IV",
-      "name" : "king grimm",
-      url: "./7.jpg"
+      id : "IV",
+      name : "king grimm",
+      url: "./challenge images/7.jpg",
+      color: "bg-gray-950"
     },
     {
-      "id" : "IV",
-      "name" : "Hollow knight",
-      url: "./8.jpg"
+      id : "IV",
+      name : "Hollow knight",
+      url: "./challenge images/8.jpg",
+      color: "bg-purple-950"
     },
     {
-      "id" : "IV",
-      "name" : "The radiance",
-      url: "./9.jpg"
+      id : "IV",
+      name : "The radiance",
+      url: "./challenge images/9.jpg",
+      color: "bg-yellow-950"
     },
   
-  ])
+  ]
 
   return (
-    <div className=' p-5 min-h-[100vh] flex flex-col items-center gap-10 '>
-      <div className=' flex items-center flex-col'>
-        <h1 className=' text-xl font-font'>Challenges</h1>
-        <img src={Down} className=' w-64' />
+    <div className=' p-5 min-h-[100vh] flex flex-col items-center gap-10  '>
+      <div className=' flex items-center justify-around w-full'>
+        <Link to={'/'}>
+          <img src={Arrow} className=' size-6' />
+        </Link>
+        <div className=' flex items-center flex-col'>
+            <h1 className=' text-xl font-font'>Challenges</h1>
+            <img src={Down} className=' w-64' />
+        </div>
+        <div className=' opacity-0'>t</div>
       </div>
-      <div className=' grid gap-12 xl:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 pb-16'>
-      {cards.map((card) => {
-        return (
-          <Card 
-          id={card.id}
-          name={card.name}
-          url={card.url}
-          color={card.color}
-          />
-        )
-      })}
+      <div>
+        <img src={Hollow} className=' w-40' />
+        <div className=' grid gap-12 xl:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1'>
+          {cards.map((card) => {
+            return (
+              <Card 
+              id={card.id}
+              name={card.name}
+              url={card.url}
+              color={card.color}
+              />
+            )
+          })}
+        </div>
+      </div>
+      <div className=' flex gap-5 pb-5 items-center justify-center'>
+        <img src={IT} className=' w-12' />
+        <img src={MC} className=' w-12' /> 
       </div>
     </div>
   );
