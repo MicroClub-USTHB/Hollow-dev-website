22fa3f1e5c4786586223c13ffe87f578f165efb7
saiber-elite <jalilotao@gmail.com>
2024-06-26 19:49:58 +0100

    added ghosts to the page

--- src/views/Challenges/card.tsx
+++ src/views/Challenges/card.tsx
@@ -8,6 +8,7 @@ export default function Card(props:any) {
       <div className=' absolute top-2 left-3 text-2xl font-sedan'>
         {props.id}
       </div>
+      <div className=' absolute top-3 right-3 text-[11px] bg-white bg-opacity-10 rounded-md px-2 py-1'>{props.difficulty}</div>
       <div className={`flex flex-col justify-center items-center absolute ${props.color} w-40 rounded-md bg-opacity-45 bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-2 `}>
         <h1 className=' font-font text-center text-sm'>{props.name}</h1>
         <img src={Down}/>
