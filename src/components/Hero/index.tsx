2714918d68ec353cef1f71706a0651cc4f295d7f
ramzy1453 <kemmounramzy93@gmail.com>
2024-06-26 20:38:37 +0100

    fixed hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,9 +5,7 @@ const Hero: React.FC = () => {
     <div className="flex flex-col items-center justify-center text-white">
       <div className="text-center mb-16">
         <h1 className="text-5xl font-bold mt-4 font-sedan">HOLLOW DEV</h1>
-        <p className="text-lg mt-2 font-sedan">
-          Let’s conquer the web kingdom!
-        </p>
+        <p className="text-lg mt-2 font-sedan">Code for the web kingdom!</p>
         <img
           src="/src/assets/hero-logo.png"
           alt="hero-logo"
@@ -20,7 +18,7 @@ const Hero: React.FC = () => {
 
       <div className="flex justify-center items-center">
         <iframe
-          src="https://drive.google.com/file/d/17QOtAYThN9_6_OR4PLUVksu4cBJb7fLw/preview"
+          src="https://drive.google.com/file/d/1XGjEAxK0dQg4LPsTj3_qk_pWsiVK-KQL/preview"
           width="800"
           height="480"
           className="border border-[#3D3D3D] rounded-xl trailer-iframe"
